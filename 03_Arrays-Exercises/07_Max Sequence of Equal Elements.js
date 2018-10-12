99862b7cf5e0bb90bf36d47fbc9367b9841a1845
kostadin-belchev <kostadin.belchev@gmail.com>
2018-10-12 11:15:56 +0300

    solve all mandatory exercises

--- 03_Arrays-Exercises/07_Max Sequence of Equal Elements.js
+++ 03_Arrays-Exercises/07_Max Sequence of Equal Elements.js
@@ -1,40 +1,27 @@
-const solve = (array = []) => {
-  let arr = array[0].split(' ').map(Number)
-  arr
+const solve = (arr = []) => {
+  let array = arr[0].split(' ').map(Number)
   let currSeqLength = 1
-  let startIndex = 0
+  // let start = 0
   let bestStart = 0
-  let bestLength = 1
+  let bestLength = 0
 
-  for (let i = 1; i < arr.length; i++) {
-    const element = arr[i];
-    element
-    if (arr[i] === arr[i - 1]) {
+  for (let i = 1; i < array.length; i++) {
+    let currElement = array[i]
+    let prevElement = array[i - 1]
+    if (currElement === prevElement) {
       currSeqLength++
-      startIndex = arr[i]
-      startIndex
+      if (currSeqLength > bestLength) {
+        bestLength = currSeqLength
+        bestStart = i - bestLength + 1
+      }
     } else {
       currSeqLength = 1
-      startIndex = arr[i]
-      startIndex
-    }
-
-    if (currSeqLength > bestLength) {
-      bestLength = currSeqLength
-      bestLength
-      bestStart = startIndex
-      bestStart
     }
   }
-
-  let result = []
-  for (let i = 0; i < bestLength; i++) {
-    result.push(bestStart)
-  }
-  return result.join(' ')
+  return array.slice(bestStart, bestStart + bestLength).join(' ')
 }
 
 console.log(solve(['2 1 1 2 3 3 2 2 2 1']))
-// console.log(solve(['1 1 1 2 3 1 3 3']))
-// console.log(solve(['4 4 4 4']))
-// console.log(solve(['0 1 1 5 2 2 6 3 3']))
+console.log(solve(['1 1 1 2 3 1 3 3']))
+console.log(solve(['4 4 4 4']))
+console.log(solve(['0 1 1 5 2 2 6 3 3']))
--- 03_Arrays-Exercises/08_Magic Sum.js
+++ 03_Arrays-Exercises/08_Magic Sum.js
@@ -1,38 +1,19 @@
-const solve = (array =[]) => {
-  let arr = array[0].split(' ').map(Number)
-  arr
-  let number = array[1]
-  number
-  let pairs = []
-  arr.forEach((element, index) => {
-    arr.forEach((element2, index) => {
-      let sum = element + element2
-      sum
-      if (sum === number) {
-        sum
-        pairs.push(`${element} ${element2}`)
+const solve = (arr =[]) => {
+  let array = arr[0].split(' ').map(Number)
+  let number = Number(arr[1])
+  for (let i = 0; i < array.length; i++) {
+    const currNumToSum = array[i];
+    for (let j = 0; j < array.length; j++) {
+      const element = array[j]
+      if (currNumToSum + element === number) {
+        console.log(`${currNumToSum} ${element}`)
       }
-    })
-  })
-  pairs
-  pairs.forEach((element, index) => {
-    let [num1, num2] = element.split(' ')
-    num1
-    num2
-    pairs.slice(index, )
-    pairs
-  })
+      delete array[i]
+      //delete array[j]
+    }
+  }
 }
 
 // solve(['1 7 6 2 19 23', 8])
-console.log(solve(['14 20 60 13 7 19 8', 27]))
-
-
-// reduce((prevValue, currValue, currIndex, array) => {
-//   prevValue
-//   currValue
-//   if (prevValue[0] === currValue[2]) {
-//     console.log(`${prevValue[0]} ${prevValue[2]}`)
-//   }
-//   return currValue
-// }/* initial value */ )
+// solve(['14 20 60 13 7 19 8', 27])
+solve(['1 2 3 4 5 6', 6])
