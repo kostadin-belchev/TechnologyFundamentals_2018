ae417f8fb5f0fc4d3dd6ae7f50640db8fb688797
Kostadin Belchev <kostadin.belchev@tick42.com>
2018-11-09 12:49:58 +0200

    finished with task 6

--- 06_Associative Arrays and Maps - Exercises/06. Travel Time.js
+++ 06_Associative Arrays and Maps - Exercises/06. Travel Time.js
@@ -1,11 +1,37 @@
 const solve = (input = ['']) => {
   const countriesToTowns = new Map()
   for (const inputString of input) {
-    const [country, town, offer] = inputString.split(' > ')
+    let [country, town, offer] = inputString.split(' > ')
+    offer = Number(offer)
     if (!countriesToTowns.has(country)) {
-      // countriesToTowns.set(country, new Map())
+      countriesToTowns.set(country, new Map())
+    }
+    const currCountry = countriesToTowns.get(country)
+    if (!currCountry.has(town)) {
+      currCountry.set(town, offer)
+    } else {
+      const lowestOffer = Math.min(offer, currCountry.get(town))
+      currCountry.set(town, lowestOffer)
     }
   }
+  const result = [...countriesToTowns].sort((a, b) => {
+    const countryAName = a[0]
+    const countryBName = b[0]
+    if (countryAName !== countryBName) {
+      return countryAName.localeCompare(countryBName)
+    }
+  })
+
+  let resultingString = ''
+  for (const countryAndTowns of result) {
+    const countryName = countryAndTowns[0]
+    resultingString += `${countryName} -> `
+    for (const keyValuePair of [...countryAndTowns[1]]) {
+      resultingString += `${keyValuePair[0]} -> ${keyValuePair[1]} `
+    }
+    resultingString += '\n'
+  }
+  console.log(resultingString)
 }
 
 solve([
